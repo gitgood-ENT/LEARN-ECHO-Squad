90e48548294489ba5e65ed734179a8bb72a26fb3
gitgood-ENT <ward.noahd@gmail.com>
2021-12-13 12:43:58 -0800

    added changes

--- week-two-stuff.js
+++ week-two-stuff.js
@@ -55,3 +55,86 @@ const upperCase = (string) => {
 const wordsUpper = words.map(upperCase)
 console.log(wordsUpper);
 
+
+
+//Kirk's run at returning the pet's name for if they are over 10 years old.
+
+// let pets = [
+//   { name: "Sheldon", type: "dog", age: 4 },
+//   { name: "Yuki", type: "cat", age: 17 },
+//   { name: "Eddie", type: "dog", age: 13 },
+//   { name: "Zoey", type: "dog", age: 2 },
+//   { name: "Artemis", type: "cat", age: 7 },
+// ];
+
+// const longTimeFriendsFilter = (array) => {
+//   return array.filter((value) => (value.age >= 10 ? value.name : null));
+// };
+// console.log(longTimeFriendsFilter(pets));
+// //-> [{ name: 'Yuki', type: 'cat', age: 17 }, { name: 'Eddie', type: 'dog', age: 13 }]
+
+// const longTimeFriendsFilter2 = (array) => {
+//   return array.filter((value) => {
+//     if (value.age >= 10) {
+//       return value.name;
+//     }
+//   });
+// };
+// console.log(longTimeFriendsFilter2(pets));
+// //-> [{ name: 'Yuki', type: 'cat', age: 17 }, { name: 'Eddie', type: 'dog', age: 13 }]
+
+// const longTimeFriendsForEach = (array) => {
+//   let oldPetNames = [];
+//   array.forEach((value) => {
+//     value.age >= 10 ? oldPetNames.push(value.name) : null;
+//   });
+//   return oldPetNames;
+// };
+// console.log(longTimeFriendsForEach(pets));
+// //-> ['Yuki', 'Eddie']
+
+
+// Create a function that returns a new array with only the names of the animals.
+
+var animals = [
+  { name: "Waffles", type: "dog", age: 12 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 },
+]
+
+console.log(animals[0].name);
+var nameArr = []
+const nameAdd = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    nameArr.push(array[i].name)
+  }
+  return nameArr
+}
+console.log(nameAdd(animals));
+
+const cats = (array) => {
+  return array.filter((value) => (value.type === "cat"));
+};
+
+
+const AnimalNames = (array) => {
+
+}
+
+var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+
+const firstUpper = (string) => {
+    //Return the lettter at index(0) capitalized and the rest of the string concatenated
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+//Got Green
+
+// declare the function capFirst that capitalizes the first letter of each string in an array
+const capFirst = (array) => {
+    array.map(firstUpper)
+}
+console.log(capFirst(randomNouns1));
